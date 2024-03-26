e8e871fe6b27667b1ff083564c2acacb5e5a7a45
youssef-hassane <youssef_hassane@icloud.com>
2024-03-26 15:12:01 +0000

    Task #1 - Fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	/* Allocate memory for the new node */
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-	
+
 	/* Check if the allocation was successful */
 	if (!node)
 	{
@@ -23,13 +23,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		/* If not, return null pointer */
 		return (theNull);
 	}
-	
+
 	/* Set the value of the node */
 	node->n = value;
-	
+
 	/* Set the parent of the node */
 	node->parent = parent;
-	
+
 	/* Initialize the left and right child pointers to null */
 	node->left = theNull;
 	node->right = theNull;
