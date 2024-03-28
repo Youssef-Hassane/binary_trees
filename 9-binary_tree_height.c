01b5afadee22f306c55c7c90a812c9a47211438b
youssef-hassane <youssef_hassane@icloud.com>
2024-03-28 13:23:33 +0000

    Task #9 - DONE

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,29 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Returns the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree
  * Return: Height of the binary tree
- * ---------------------
+ * ---------------------------
  * Prototype: size_t binary_tree_height(const binary_tree_t *tree)
- * ----------------------
+ * ---------------------------
  * @tree: Pointer to the root node of the tree to traverse
- * ------------------
+ * ---------------------------
  * By Youssef Hassane
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
-	/* Base case: tree is empty */
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
 	}
-
-	/* Recursive calculation of left and right subtree heights */
-	size_t leftHeight = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
-	size_t rightHeight = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
-
-	/* Return the larger of the two subtree heights */
-	return ((leftHeight > rightHeight) ? leftHeight : rightHeight);
+	return (0);
 }
